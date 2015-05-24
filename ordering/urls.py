4fd521ff599863e80f572f14a6da6e3ce8c09eac
Laurent Peuch <cortex@worlddomination.be>
2015-05-24 17:13:09 +0200

    [enh] prepare ordering view

--- ordering/urls.py
+++ ordering/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('ordering.views',
     url(r'^$', TemplateView.as_view(template_name="home.haml"), name='home'),
+    url(r'^order$', 'make_order', name='order'),
 )
