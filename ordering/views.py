33a8212215f0da56d3478661d214ebca834446e8
Laurent Peuch <cortex@worlddomination.be>
2015-05-25 00:11:51 +0200

    [enh] order form is working \o/

--- ordering/views.py
+++ ordering/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
+from django.db import transaction
 
-from .models import Component
+from .models import Component, ComponentOrder
 from .forms import OrderForm
 
 
 def make_order(request):
-    return render(request, "order.haml", {
-        "form": OrderForm(),
-        "default_components": Component.objects.filter(in_default_pack=True),
-        "other_components": Component.objects.filter(in_default_pack=False),
-    })
+    if request.method == "GET":
+        return render(request, "order.haml", {
+            "form": OrderForm(),
+            "default_components": Component.objects.filter(in_default_pack=True),
+            "other_components": Component.objects.filter(in_default_pack=False),
+        })
+
+    assert request.method == "POST"
+
+    form = OrderForm(request.POST)
+
+    print request.POST
+
+    if not form.is_valid():
+        return render(request, "order.haml", {
+            "form": form,
+            "default_components": Component.objects.filter(in_default_pack=True),
+            "other_components": Component.objects.filter(in_default_pack=False),
+        })
+
+    with transaction.atomic():
+        order = form.save()
+
+        for component in Component.objects.all():
+            if form.cleaned_data["component_%d_number" % component.id] > 0:
+                ComponentOrder.objects.create(
+                    order=order,
+                    component=component,
+                    number=form.cleaned_data["component_%d_number" % component.id]
+                )
+
+    # TODO send email
+
+    return render(request, "success.haml", {"order": order})
