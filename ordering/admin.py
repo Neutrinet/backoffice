ae520f7b0485496cff310b794233cf022a9b2777
Laurent Peuch <cortex@worlddomination.be>
2015-05-24 06:42:03 +0200

    [enh] organize information on ordre admin page

--- ordering/admin.py
+++ ordering/admin.py
@@ -5,7 +5,27 @@ from .models import Order, Component
 
 
 class OrderAdmin(reversion.VersionAdmin):
-    pass
+    readonly_fields = ('made_on',)
+    fieldsets = (
+        (None, {
+            'fields': ('made_on',)
+        }),
+        ('Member infos', {
+            'fields': (('nick', 'email'), ('first_name', 'last_name'))
+        }),
+        ('Member preferences', {
+            'fields': (('wants_vpn', 'wants_to_install_everything_himself'),)
+        }),
+        ('VPN informations', {
+            'fields': (('street', 'postal_code'), ('town', 'country'), ('birthplace', 'birth_date'))
+        }),
+        ('Status informations', {
+            'fields': (('estimated_price', 'real_price'), ('has_payed', 'price_payed'), ('member_has_been_give_order', 'we_have_received_the_order'))
+        }),
+        ('Additional informations', {
+            'fields': ('comment', 'private_comment'),
+        }),
+    )
 
 
 class ComponentAdmin(reversion.VersionAdmin):
