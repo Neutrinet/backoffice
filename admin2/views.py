e880c1868399768fbe11434a4f15c1064157a661
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 08:14:42 +0200

    [enh] start the 'current_order_page'

--- admin2/views.py
+++ admin2/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Sum
 
-# Create your views here.
+from ordering.models import Order
+
+
+def current_order(request):
+    return render(request, "admin2/current_order.haml", {
+        "orders": Order.objects.filter(member_has_been_give_order=False),
+        "total_estimated_price": Order.objects.aggregate(Sum('estimated_price'))['estimated_price__sum'],
+    })
