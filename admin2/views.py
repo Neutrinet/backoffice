33c71c598193df92ad4417a8f614bb54b9d34c22
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 08:50:12 +0200

    [enh] display number or ordered components

--- admin2/views.py
+++ admin2/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.db.models import Sum
 
-from ordering.models import Order
+from ordering.models import Order, Component
 
 
 def current_order(request):
     return render(request, "admin2/current_order.haml", {
         "orders": Order.get_current(),
+        "components": Component.objects.filter(componentorder__order=Order.get_current()).distinct(),
         "total_estimated_price": Order.objects.aggregate(Sum('estimated_price'))['estimated_price__sum'],
     })
