c3cc1b07fa1159b41a96b20d4ec54666c066ecdc
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 08:42:04 +0200

    [mod] small refactor

--- admin2/views.py
+++ admin2/views.py
@@ -6,6 +6,6 @@ from ordering.models import Order
 
 def current_order(request):
     return render(request, "admin2/current_order.haml", {
-        "orders": Order.objects.filter(member_has_been_give_order=False),
+        "orders": Order.get_current(),
         "total_estimated_price": Order.objects.aggregate(Sum('estimated_price'))['estimated_price__sum'],
     })
