76af492ff5b7595ebe8f8bbecba64d1c83409708
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 08:05:00 +0200

    [enh] add home to admin2

--- admin2/urls.py
+++ admin2/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+from .utils import user_is_admin
 
 
 urlpatterns = patterns('admin2.views',
-    # url(r'^$', , name=''),
+    url(r'^$', user_is_admin(TemplateView.as_view(template_name="admin2/home.haml")), name='admin2_home'),
 )
