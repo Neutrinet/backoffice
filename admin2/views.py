fa59a1397920371199cfaca8795a042867b97d20
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 09:42:04 +0200

    [enh] display number of new vpn subscription

--- admin2/views.py
+++ admin2/views.py
@@ -7,6 +7,7 @@ from ordering.models import Order, Component
 def current_order(request):
     return render(request, "admin2/current_order.haml", {
         "orders": Order.get_current(),
+        "new_vpn_subscription": Order.get_current().filter(wants_vpn=True).count(),
         "components": Component.objects.filter(componentorder__order=Order.get_current()).distinct(),
         "total_estimated_price": Order.objects.aggregate(Sum('estimated_price'))['estimated_price__sum'],
     })
