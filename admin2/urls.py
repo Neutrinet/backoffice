3435e4e675448c89195ab1dca3e9f77b163353ff
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 08:07:00 +0200

    [enh] link to current order view

--- admin2/urls.py
+++ admin2/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 from .utils import user_is_admin
+from . import views
 
 
 urlpatterns = patterns('admin2.views',
     url(r'^$', user_is_admin(TemplateView.as_view(template_name="admin2/home.haml")), name='admin2_home'),
+    url(r'^current_order/$', user_is_admin(views.current_order), name='admin2_current_order'),
 )
