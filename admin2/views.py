4217e62c7aaf9f4476446a1e2dd556ebf8ec57a9
Laurent Peuch <cortex@worlddomination.be>
2015-06-11 11:20:14 +0200

    [enh] grouper orders by providers

--- admin2/views.py
+++ admin2/views.py
@@ -5,9 +5,21 @@ from ordering.models import Order, Component
 
 
 def current_order(request):
+    components = Component.objects.filter(componentorder__order=Order.get_current()).distinct()
+
+    providers = set()
+    for component in components:
+        providers.add(component.provider)
+
+    provider_order_list = [{
+        "provider": provider,
+        "orders": Order.get_current().filter(componentorder__component__url__icontains=provider),
+    } for provider in providers]
+
     return render(request, "admin2/current_order.haml", {
         "orders": Order.get_current(),
         "new_vpn_subscription": Order.get_current().filter(wants_vpn=True).count(),
-        "components": Component.objects.filter(componentorder__order=Order.get_current()).distinct(),
+        "components": components,
         "total_estimated_price": Order.objects.aggregate(Sum('estimated_price'))['estimated_price__sum'],
+        "provider_order_list": provider_order_list,
     })
