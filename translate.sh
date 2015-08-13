fec33b6bd2e99cec7bed3b1d0738f2bc26fcb074
Laurent Peuch <cortex@worlddomination.be>
2015-08-13 02:48:29 +0200

    [mod] update all translation files

--- translate.sh
+++ translate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python manage.py makemessages -i ve -l fr --settings=cube_ordering_interface.settings --extension haml,html,txt,py
+python manage.py makemessages -i ve -a --settings=cube_ordering_interface.settings --extension haml,html,txt,py
