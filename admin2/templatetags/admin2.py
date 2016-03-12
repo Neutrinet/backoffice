3ee96ad6e5149b6f63a209a155f633385d5413ca
Laurent Peuch <cortex@worlddomination.be>
2016-03-12 04:43:41 +0100

    [fix] can't substract None

--- admin2/templatetags/admin2.py
+++ admin2/templatetags/admin2.py
@@ -33,6 +33,8 @@ def total_addition_from_free_price(orders):
     estimated_price__sum = orders.filter(price_payed__isnull=False).aggregate(Sum('estimated_price'))["estimated_price__sum"]
     price_payed__sum = orders.filter(price_payed__isnull=False).aggregate(Sum('price_payed'))["price_payed__sum"]
 
+    if price_payed__sum is None and estimated_price__sum is None:
+        return 0
     if real_price__sum is None:
         return price_payed__sum - estimated_price__sum
     else:
