e1f77ce1071ea7f296a98f81d804021e0ae6d40d
Laurent Peuch <cortex@worlddomination.be>
2016-04-24 07:01:12 +0200

    [mod] django_admin_bootstrapped is incompatible with admin_views

--- cube_ordering_interface/settings.py
+++ cube_ordering_interface/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'admin_views',
     'bootstrap3',
-    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
