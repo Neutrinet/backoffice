811d2266bcd7c1180490a63e9ef8f68e1f6e4010
Laurent Peuch <cortex@worlddomination.be>
2016-04-24 09:49:12 +0200

    [enh] new filter to see who is late for VPN

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+from datetime import datetime
+
 from reversion.admin import VersionAdmin
 
 from django.contrib import admin
@@ -25,9 +27,29 @@ class IsStillMember(admin.SimpleListFilter):
             return queryset.filter(member_end__isnull=False)
 
 
+class IsLateOnVpn(admin.SimpleListFilter):
+    title = 'Est en retard d\'abonnement'
+    parameter_name = 'is_late_on_vpn_subscription'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Oui'),
+            ('no', 'Non'),
+        )
+
+    def queryset(self, request, queryset):
+        today = datetime.today()
+
+        if self.value() == 'yes':
+            return queryset.filter(vpn_is_paid_until__lt=today, vpn=True)
+
+        if self.value() == 'no':
+            return queryset.filter(vpn_is_paid_until__gte=today, vpn=True)
+
+
 class MemberAdmin(VersionAdmin):
     list_display = ('first_name', 'last_name', 'last_paid_date', 'vpn_is_paid_until', 'email', 'vpn', 'cube', 'is_not_a_member_yet')
-    list_filter = ('last_paid_date', 'ca_member', IsStillMember, 'vpn', 'cube', 'is_not_a_member_yet')
+    list_filter = ('last_paid_date', 'ca_member', IsStillMember, IsLateOnVpn, 'vpn', 'cube', 'is_not_a_member_yet')
 
     radio_fields = {"juridical_form": admin.HORIZONTAL}
 
