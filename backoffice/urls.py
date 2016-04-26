b2b7212b966edad565b39d384297084ac99c1a05
Laurent Peuch <cortex@worlddomination.be>
2016-04-26 14:00:45 +0200

    [fix] reactivate ffdnapi

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,9 +1,13 @@
+from members.views import ffdn_api
+
 from django.conf.urls import include, url
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin2/', include('admin2.urls')),
     url(r'^', include('ordering.urls')),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^isp.json$', ffdn_api),
 ]
