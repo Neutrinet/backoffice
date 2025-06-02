8ae04b3d3051c7cd788a3d0d9a12822acb562335
HgO <hgo@batato.be>
2025-06-02 11:00:00 +0200

    upgrade to django 1.11

--- admin2/urls.py
+++ admin2/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import ListView
 
 from ordering.models import GroupOrder
@@ -7,7 +7,7 @@ from .utils import user_is_admin
 from . import views
 
 
-urlpatterns = patterns('admin2.views',
+urlpatterns = [
     url(r'^$', user_is_admin(views.dashboard), name='admin2_home'),
     url(r'^group_order/(?P<pk>\d+)/$', user_is_admin(views.group_order_detail), name='admin2_group_order_detail'),
-)
+]
--- ordering/urls.py
+++ ordering/urls.py
@@ -1,16 +1,17 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from . import views
 
-urlpatterns = patterns('ordering.views',
-    url(r'^$', 'home', name='home'),
-    url(r'^order$', 'make_order', name='order'),
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^order$', views.make_order, name='order'),
     url(r'^success$', TemplateView.as_view(template_name="success.haml"), name='success'),
-)
+]
 
 if settings.DEBUG:
-    urlpatterns += patterns('ordering.views',
-        url(r'^email_debug/(?P<pk>\d+)/$', 'email_debug', name='email_debug'),
-        url(r'^admin_email_debug/(?P<pk>\d+)/$', 'admin_email_debug', name='admin_email_debug'),
-    )
+    urlpatterns += [
+        url(r'^email_debug/(?P<pk>\d+)/$', views.email_debug, name='email_debug'),
+        url(r'^admin_email_debug/(?P<pk>\d+)/$', views.admin_email_debug, name='admin_email_debug'),
+    ]
