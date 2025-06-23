eb427178644ba6726bfafa5e187163f966949b87
HgO <hgo@batato.be>
2025-06-23 13:57:34 +0200

    translations

--- ordering/models.py
+++ ordering/models.py
@@ -52,7 +52,7 @@ class GroupOrder(models.Model):
                 days=31
             )
 
-        return _("Group Order #%s %s") % (number, next_month.strftime("%B %Y"))
+        return _("Group Order #{number} {date}").format(number=number, date=next_month.strftime("%B %Y"))
 
     @classmethod
     def close_deadline_passed_grouper_order(klass):
@@ -201,14 +201,14 @@ class Order(models.Model):
         return self.has_a_working_cube or self.wants_to_install_everything_himself
 
     def __str__(self):
-        return _("order #%s for %s made %s days ago") % (
-            self.id,
-            (
+        return _("order #{number} for {name} made {days} days ago").format(
+            number=self.id,
+            name=(
                 "%s %s" % (self.first_name, self.last_name)
                 if not self.nick
                 else "%s %s (%s)" % (self.first_name, self.last_name, self.nick)
             ),
-            (datetime.now() - self.made_on.replace(tzinfo=None)).days,
+            days=(datetime.now() - self.made_on.replace(tzinfo=None)).days,
         )
 
     def save(self, *args, **kwargs):
